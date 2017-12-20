a4049b4cc8a3df7e1cac66de72e1ade4f2570b4d
TovarichMFS <calmels.dimitri@gmail.com>
2017-12-20 16:05:03 +0100

    Ajout deadline Task

--- src/Model/Task.java
+++ src/Model/Task.java
@@ -1,14 +1,18 @@
 package Model;
 
+import java.util.Date;
+
 public final class Task {
     private final long id;
     private final String description;
     private boolean done;
+    private Date deadline;
 
-    public Task(long id, String description, boolean done) {
+    public Task(long id, String description, boolean done, Date pfDeadline) {
         this.id = id;
         this.description = description;
         this.done = done;
+        this.deadline = pfDeadline;
     }
 
     public long getId() {
@@ -25,5 +29,13 @@ public final class Task {
 
     public void setDone(boolean done) {
         this.done = done;
+    }
+    
+    public Date getDeadLine() {
+    	return this.deadline;
+    }
+    
+    public void setDeadLine(Date pfDeadline) {
+    	this.deadline = pfDeadline;
     }
 }
