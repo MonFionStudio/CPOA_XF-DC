1a099dbf6ab73dd224a5c4b8518b4fe3876bfeb0
TovarichMFS <calmels.dimitri@gmail.com>
2017-12-20 15:11:37 +0100

    Suce moi

---
+++ src/Model/Project.java
@@ -0,0 +1,11 @@
+package Model;
+
+import java.util.LinkedHashMap;
+import java.util.List;
+import java.util.Map;
+
+
+public class Project {
+	private final Map<String, List<Task>> tasks = new LinkedHashMap<>();
+
+}
