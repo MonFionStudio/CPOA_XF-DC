f51fb5cf31041d2f8eb461db41e17fdaeb386cf7
Xavier Fernandez <xavier.fernandez81000@gmail.com>
2017-12-20 17:12:01 +0100

    commit

--- src/Controler/Routeur.java
+++ src/Controler/Routeur.java
@@ -1,5 +1,6 @@
 package Controler;
 
+import java.util.ArrayList;
 import java.util.Date;
 import java.util.List;
 
@@ -9,69 +10,94 @@ import View.TaskOtron3000;
 
 public class Routeur {
 	private Controler ctrl;
-	private List<Project> lProject;
+	private List<Project> lProject = new ArrayList<Project>();
 	private TaskOtron3000 view;
 	private int lastID = 0;
-	
+
 	public Routeur(TaskOtron3000 pfView) {
 		this.view = pfView;
 	}
-	
+
 	public void execute(String commandLine) {
-        String[] commandRest = commandLine.split(" ");
-        String command = commandRest[0];
-        switch (command) {
-            case "show":
-            	String msg = "";
-                for (Project p : this.lProject) {
-					msg += p.getNom();
-					for (Task task : p.getTasks()) {
-						if(task.isDone())
-							msg += "    [x] " + task.getId() + ": " + task.getDescription() + "\n";
-						else
-							msg += "    [ ] " + task.getId() + ": " + task.getDescription() + "\n";
-					}
-					view.refresh(msg);
+		String[] commandRest = commandLine.split(" ");
+		String command = commandRest[0];
+		switch (command) {
+		case "show":
+			String msg = "";
+			for (Project p : this.lProject) {
+				msg += p.getNom();
+				for (Task task : p.getTasks()) {
+					if(task.isDone())
+						msg += "    [x] " + task.getId() + ": " + task.getDescription() + "\n";
+					else
+						msg += "    [ ] " + task.getId() + ": " + task.getDescription() + "\n";
 				}
-                break;
-            case "add":
-            	if (commandRest[1].equals("project")) {
-            		try {
-            			lProject.add(new Project(commandRest[2]));
-            		}catch (Exception e) {
+				view.refresh(msg);
+			}
+			break;
+		case "add":
+			try {
+				if (commandRest[1].equals("project")) {
+					try {
+						lProject.add(new Project(commandRest[2]));
+					}catch (Exception e) {
 						view.refresh("add <project> <Project Name>");
 					}
-                } else if (commandRest[1].equals("task")) {
-                    try {
-                    	boolean bon = false;
+				} else if (commandRest[1].equals("task")) {
+					try {
 						for (Project p : this.lProject) {
 							if(p.getNom().equals(commandRest[2])) {
-								p.addTask(new Task(lastID, commandRest[3], false, new Date(commandRest[4])));
+								String[] date = commandRest[4].split("-");
+								p.addTask(new Task(lastID, commandRest[3], false, new Date(Integer.parseInt(date[3]), Integer.parseInt(date[2]), Integer.parseInt(date[1]))));
 								lastID++;
 							}
 						}
-						if(!bon)
-							view.refresh("Projet inexistant !");
 					} catch (Exception e) {
-						view.refresh("add <task> <Project Name> <Task Description> <DeadLine>");
+						view.refresh("add <task> <Project Name> <Task Description> <dd/mm/YYYY>");
 					}
-                }
-            	view.refresh("add <project|task> <Project Name> <Task Description>");
-                break;
-            case "check":
-                for(Project p : this.lProject) {
-                	p.getTask(Integer.parseInt(commandRest[1]));
-                }
-                break;
-            case "uncheck":
-                uncheck(commandRest[1]);
-                break;
-            case "help":
-                help();
-                break;
-            default:
-                error(command);
-                break;
-        }
-    }
+				}
+			} catch (Exception e) {
+				view.refresh("add <project|task> <Project Name> <Task Description>");
+			}
+			break;
+		case "check":
+			try {
+				boolean bonc = false;
+				for(Project p : this.lProject) {
+					if(p.getTask(Integer.parseInt(commandRest[1])) != null) {
+						p.getTask(Integer.parseInt(commandRest[1])).setDone(true);
+						bonc = true;
+						break;
+					}
+				}
+				if(!bonc)
+					view.refresh("Erreur la tache est inexistante !");
+			}catch (Exception e) {
+				view.refresh("Erreur lors du check !");
+			}
+			break;
+		case "uncheck":
+			try {
+				boolean bonuc = false;
+				for(Project p : this.lProject) {
+					if(p.getTask(Integer.parseInt(commandRest[1])) != null) {
+						p.getTask(Integer.parseInt(commandRest[1])).setDone(false);
+						bonuc = true;
+						break;
+					}
+				}
+				if(!bonuc)
+					view.refresh("Erreur la tache est inexistante !");
+			} catch (Exception e) {
+				view.refresh("Erreur lors du uncheck !");
+			}
+			break;
+		case "help":
+			view.help();
+			break;
+		default:
+			view.error(command);
+			break;
+		}
+	}
 }
--- src/View/TaskOtron3000.java
+++ src/View/TaskOtron3000.java
@@ -4,14 +4,8 @@ import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.io.PrintWriter;
-import java.util.ArrayList;
-import java.util.LinkedHashMap;
-import java.util.List;
-import java.util.Map;
 
 import Controler.Routeur;
-import Model.Project;
-import Model.Task;
 
 public final class TaskOtron3000 implements Runnable {
     private static final String QUIT = "quit";
@@ -19,8 +13,6 @@ public final class TaskOtron3000 implements Runnable {
     private Routeur routeur = new Routeur(this);
     private final BufferedReader in;
     private final PrintWriter out;
-
-    private long lastId = 0;
 
     public static void main(String[] args) throws Exception {
         BufferedReader in = new BufferedReader(new InputStreamReader(System.in));
@@ -50,60 +42,7 @@ public final class TaskOtron3000 implements Runnable {
         }
     }
 
-    
-
-    private void show() {
-        for (Project project : this.projects) {
-            out.println(project.getNom());
-            for (Task task : project.getTasks()) {
-                out.printf("    [%c] %d: %s%n", (task.isDone() ? 'x' : ' '), task.getId(), task.getDescription());
-            }
-            out.println();
-        }
-    }
-
-    private void add(String commandLine) {
-        String[] subcommandRest = commandLine.split(" ", 2);
-        String subcommand = subcommandRest[0];
-        if (subcommand.equals("project")) {
-            addProject(subcommandRest[1]);
-        } else if (subcommand.equals("task")) {
-            String[] projectTask = subcommandRest[1].split(" ", 2);
-            addTask(projectTask[0], projectTask[1]);
-        }
-    }
-
-    private void addProject(String name) {
-        this.projects.add(new Project(name));
-    }
-
-    private void addTask(String project, String description) {
-        Routeur.addTask
-    }
-
-    private void check(String idString) {
-        setDone(idString, true);
-    }
-
-    private void uncheck(String idString) {
-        setDone(idString, false);
-    }
-
-    private void setDone(String idString, boolean done) {
-        int id = Integer.parseInt(idString);
-        for (Map.Entry<String, List<Task>> project : tasks.entrySet()) {
-            for (Task task : project.getValue()) {
-                if (task.getId() == id) {
-                    task.setDone(done);
-                    return;
-                }
-            }
-        }
-        out.printf("Could not find a task with an ID of %d.", id);
-        out.println();
-    }
-
-    private void help() {
+    public void help() {
         out.println("Commands:");
         out.println("  show");
         out.println("  add project <project name>");
@@ -113,17 +52,13 @@ public final class TaskOtron3000 implements Runnable {
         out.println();
     }
 
-    private void error(String command) {
+    public void error(String command) {
         out.printf("I don't know what the command \"%s\" is.", command);
         out.println();
     }
 
-    private long nextId() {
-        return ++lastId;
-    }
-
 	public void refresh(String msg) {
-		out.print(msg);
+		out.print(msg + "\n");
         out.flush();
 	}
 }
